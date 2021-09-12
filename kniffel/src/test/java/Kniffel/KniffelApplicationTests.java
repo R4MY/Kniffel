98e645f372341ed03fe8a685d1510d4d3ed5f263
Ramy Faris <ramy1@gmx.at>
2021-09-12 19:38:06 +0200

    Initial Commit.

---
+++ kniffel/src/test/java/Kniffel/KniffelApplicationTests.java
@@ -0,0 +1,13 @@
+package Kniffel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KniffelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
